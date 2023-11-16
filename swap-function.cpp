3f03b7a9afec8066b2815bba07e5b0d62fc8d024
PetarGA <123602571+PetarGA@users.noreply.github.com>
2023-11-16 10:56:23 +0000

    last commit

--- swap-function.cpp
+++ swap-function.cpp
@@ -2,4 +2,34 @@
 #include <array> 
 
 using namespace std; 
+//declare the function array with 3 arguments 
+void swap_array(int arr[], int x, int y);
 
+int main () {
+
+// create an array in the main method 
+int numbers[] = {1, 2, 3};
+cout << "Now will be swapped the numbers inside the array. ";
+//print the first square brackets  
+cout << " [ ";
+// call the function and assign variables to it 
+swap_array(numbers, 0, 1);
+// go through the for loop to swap the numbers
+for ( int i = 0; i < 3; i++){
+    cout << numbers[i];
+    // assign a comma after the number 
+    if ( i < 2){
+        cout<<", ";
+    }
+}
+// print the closing square bracket
+cout << " ]";
+return 0;
+}
+// definiton for the swap function
+// assign a temporary variable and attached the arguments to it 
+void swap_array(int arr[], int x, int y){
+int temp = arr[x];
+arr[x] = arr[y];
+arr[y] = temp;
+}
