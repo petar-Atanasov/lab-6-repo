df0b2975e7a017139e87a4c294b920b3d3d3dada
PetarGA <123602571+PetarGA@users.noreply.github.com>
2023-11-16 10:30:36 +0000

    message

--- print-array.cpp
+++ print-array.cpp
@@ -2,16 +2,17 @@
 #include <array>
 
 using namespace std;
-
+// create a function for array with two arguments
 void print_Array(int arr[], int size );
 
 int main() { 
-
+    
     int numbers[] = {1, 2, 3};
+    // call the function in the main method
     print_Array(numbers, 3);
     return 0;
 }
-
+// definiton of the funciton
 void print_Array(int arr[], int size){
     cout << " [ ";
 
